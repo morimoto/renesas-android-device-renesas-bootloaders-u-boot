01c467516cb196a1361c38cdb83ac7715a5e2b24
Vitaliy Vasylskyy <vitaliy.vasylskyy@globallogic.com>
2018-11-08 10:14:40 +0200

    Move bootloader partitions to mmcblk0boot0\1 hw parts
    Change-Id: Ic0e820e30bf5b87aaae16b6265c3f407adcb2ece
    Signed-off-by: Leonid Komaryanskiy <leonid.komaryanskiy@globallogic.com>
    Signed-off-by: Vitaliy Vasylskyy <vitaliy.vasylskyy@globallogic.com>

--- common/avb_verify.c
+++ common/avb_verify.c
@@ -1041,6 +1041,11 @@ int avb_is_slot_bootable(unsigned int slot, bool *bootable)
 	return 0;
 }
 
+int fastboot_set_active_slot(int slot_idx)
+{
+	return 0;
+}
+
 const char *cb_get_slot_char(void)
 {
     static const char *cur_slot = NULL;
