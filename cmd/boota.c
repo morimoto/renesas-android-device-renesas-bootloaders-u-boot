32e44ff5db370cfcfb944f82c8c4990a740462d9
Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>
2018-11-26 12:31:40 +0200

    Added product partition and DTB overlay
    Change-Id: Ie5c2171b49b0be127ccdfeba6a467643479f9cef
    Signed-off-by: Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>

--- cmd/boota.c
+++ cmd/boota.c
@@ -51,6 +51,7 @@
 #endif
 
 #define	ADSP_DTBO_NUM			4
+#define	PARTITIONS_DTBO_NUM		5
 
 /* Unpacked kernel image size must be no more than
  * andr_img_hdr.ramdisk_addr -  andr_img_hdr.kernel_addr,
@@ -646,6 +647,11 @@ static int load_dt_with_overlays(struct fdt_header *load_addr,
 	dmbo_indx[dmbo_count] = ADSP_DTBO_NUM;
 	++dmbo_count;
 #endif /* defined(ENABLE_ADSP) */
+
+#if defined(ENABLE_PRODUCT_PART)
+	dmbo_indx[dmbo_count] = PARTITIONS_DTBO_NUM;
+	++dmbo_count;
+#endif /* defined(ENABLE_PRODUCT_PART) */
 
 	add_dtbo_index(dmbo_indx, dmbo_count);
 
