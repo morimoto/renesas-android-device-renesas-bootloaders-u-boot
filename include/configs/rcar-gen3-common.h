2d8920a7d2aefc546de7bbc70a94280c6eb9fe0e
Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>
2018-11-30 14:53:18 +0200

    U-boot: LPDDR4: implemented run-time detection of the DDR configuration
    Getting from BL2 the current memory configuration.
    
    Change-Id: Ieb275d9cab07fb8477732678262f3c311f21b039
    Signed-off-by: Sergii Boryshchenko <sergii.boryshchenko@globallogic.com>
    Signed-off-by: Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>

--- include/configs/rcar-gen3-common.h
+++ include/configs/rcar-gen3-common.h
@@ -52,6 +52,11 @@
 #define CONFIG_SYS_BOOTMAPSZ		(8 * 1024 * 1024)
 #define CONFIG_SYS_BOOTM_LEN		(64 << 20)
 
+/*This values are used for RAM autodetection only*/
+#define	BOARDCNF_8GBIT_1RANK	7	/* Board with H3 Ver.2.0 or later/SIP(8Gbit 1rank) */
+#define	BOARDCNF_8GBIT_2RANK	8	/* Board with H3 Ver.2.0 or later/SIP(8Gbit 2rank) */
+
+
 /* ENV setting */
 #define CONFIG_ENV_OVERWRITE
 #define CONFIG_ENV_SECT_SIZE	(128 * 1024)
