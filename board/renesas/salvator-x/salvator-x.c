a853d917615e463d9f704e3a10b8381d64233134
Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>
2019-06-19 14:51:05 +0300

    [Bugfix] Fixed correct selection of FIT config by M3N board
    Signed-off-by: Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>
    Change-Id: Idf6e69290c3f1a093934b946b2aa11fbc27339f0
    Jira-ID: ADM-3372

--- board/renesas/salvator-x/salvator-x.c
+++ board/renesas/salvator-x/salvator-x.c
@@ -201,12 +201,10 @@ int board_fit_config_name_match(const char *name)
 #if defined(CONFIG_R8A7795)
 		if (!strcmp(dt_fit.target_name, "r8a7795-salvator-x-u-boot"))
 			return 0;
-#endif
-#if defined(CONFIG_R8A7796)
+#else /*defined(CONFIG_R8A7796)*/
 		if (!strcmp(dt_fit.target_name, "r8a7796-salvator-x-u-boot"))
 			return 0;
-#endif
-#if defined(CONFIG_R8A77965)
+
 		if (!strcmp(dt_fit.target_name, "r8a77965-salvator-x-u-boot"))
 			return 0;
 #endif
@@ -233,8 +231,7 @@ int board_fit_config_name_match(const char *name)
 			}
 		}
 		/* else works default : return -1 */
-#endif
-#if defined(CONFIG_R8A7796)
+#else /*defined(CONFIG_R8A7796)*/
 		if (!ret) {
 			if (!strcmp(dt_fit.target_name,
 				    "r8a7796-salvator-xs-u-boot") &&
@@ -249,8 +246,6 @@ int board_fit_config_name_match(const char *name)
 			}
 		}
 		/* else works default : return -1 */
-#endif
-#if defined(CONFIG_R8A77965)
 		if (!strcmp(dt_fit.target_name, "r8a77965-salvator-xs-u-boot"))
 			return 0;
 #endif
