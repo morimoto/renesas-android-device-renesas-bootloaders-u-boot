111ba32cbbc08d80a1b6122a4cfac66b6b7725bb
Sergii Boryshchenko <sergii.boryshchenko@globallogic.com>
2019-08-16 18:47:47 +0300

    Revert "SMC: workaround of the improper mmap table settings"
    This reverts commit 6cfa225730d36948a18eae642a5be6130f21908e.
    
    Jira-Id: ADM-3154
    Change-Id: I55e5a5afc613ee107818ffd9198d3f5850026ed1
    Signed-off-by: Sergii Boryshchenko <sergii.boryshchenko@globallogic.com>

--- board/renesas/salvator-x/salvator-x.c
+++ board/renesas/salvator-x/salvator-x.c
@@ -179,7 +179,12 @@ void board_cleanup_before_linux(void)
 int board_fit_config_name_match(const char *name)
 {
 	int ret;
-	struct rcar_dram_tiny_t dram_tiny;
+#if defined(CONFIG_R8A7795) || defined(CONFIG_R8A7796)
+	int i;
+	int bank_num;
+	u64 bank_size;
+#endif
+	struct rcar_dram_conf_t dram_conf_addr;
 	struct rcar_dt_fit_t dt_fit;
 
 	dt_fit.board_id = 0xff;
@@ -194,7 +199,7 @@ int board_fit_config_name_match(const char *name)
 	if (ret)
 		return -1;
 
-	ret = board_detect_dram2(&dram_tiny);
+	ret = board_detect_dram(&dram_conf_addr);
 
 	switch (dt_fit.board_id) {	 /* only supported board */
 	case BOARD_ID_SALVATOR_X:
@@ -215,20 +220,25 @@ int board_fit_config_name_match(const char *name)
 #if defined(CONFIG_R8A7795)
 		if (!ret) {
 			/* select memory type */
+			bank_num = 0;
+			for (i = 0; i < 4; i++) {
+				if (dram_conf_addr.address[i])
+					bank_num++;
+				else
+					break;
+			};
+			bank_size = dram_conf_addr.size[0];
 			if (!strcmp(dt_fit.target_name,
 				    "r8a7795-salvator-xs-u-boot") &&
-				dram_tiny.bank_num == 4 &&
-				dram_tiny.bank_size == 0x40000000) {
+			    bank_num == 4 && bank_size == 0x40000000) {
 				return 0;
 			} else if (!strcmp(dt_fit.target_name,
 					   "r8a7795-salvator-xs-4x2g-u-boot") &&
-				dram_tiny.bank_num == 4 &&
-				dram_tiny.bank_size == 0x80000000) {
+				   bank_num == 4 && bank_size == 0x80000000) {
 				return 0;
 			} else if (!strcmp(dt_fit.target_name,
 					   "r8a7795-salvator-xs-2x2g-u-boot") &&
-				dram_tiny.bank_num == 2 &&
-				dram_tiny.bank_size == 0x80000000) {
+				   bank_num == 2 && bank_size == 0x80000000) {
 				return 0;
 			}
 		}
@@ -236,15 +246,21 @@ int board_fit_config_name_match(const char *name)
 #endif
 #if defined(CONFIG_R8A7796)
 		if (!ret) {
-			if (!strcmp(dt_fit.target_name,
-				    "r8a7796-salvator-xs-u-boot") &&
-				dram_tiny.bank_num == 2 &&
-				dram_tiny.bank_size == 0x80000000) {
+			/* select memory type */
+			bank_num = 0;
+			for (i = 0; i < 4; i++) {
+				if (dram_conf_addr.address[i])
+					bank_num++;
+				else
+					continue;
+			}
+			bank_size = dram_conf_addr.size[0];
+			if (!strcmp(dt_fit.target_name, "r8a7796-salvator-xs-u-boot") &&
+			    bank_num == 2 && bank_size == 0x80000000) {
 				return 0;
 			} else if (!strcmp(dt_fit.target_name,
 					   "r8a7796-salvator-xs-2x4g-u-boot") &&
-				dram_tiny.bank_num == 2 &&
-				dram_tiny.bank_size == 0x100000000) {
+				   bank_num == 2 && bank_size == 0x100000000) {
 				return 0;
 			}
 		}
--- board/renesas/ulcb/ulcb.c
+++ board/renesas/ulcb/ulcb.c
@@ -164,7 +164,10 @@ void board_cleanup_before_linux(void)
 int board_fit_config_name_match(const char *name)
 {
 	int ret;
-	struct rcar_dram_tiny_t dram_tiny;
+	int i;
+	int bank_num;
+	u64 bank_size;
+	struct rcar_dram_conf_t dram_conf_addr;
 	struct rcar_dt_fit_t dt_fit;
 
 	dt_fit.board_id = 0xff;
@@ -179,22 +182,28 @@ int board_fit_config_name_match(const char *name)
 	if (ret)
 		return -1;
 
-	ret = board_detect_dram2(&dram_tiny);
+	ret = board_detect_dram(&dram_conf_addr);
 
 	switch (dt_fit.board_id) {
 	case BOARD_ID_SK_PREM:
 		if (!ret) {
 			/* select memory type */
+			bank_num = 0;
+			for (i = 0; i < 4; i++) {
+				if (dram_conf_addr.address[i])
+					bank_num++;
+				else
+					break;
+			}
+			bank_size = dram_conf_addr.size[0];
 #if defined(CONFIG_R8A7795)
 			if (!strcmp(dt_fit.target_name,
 				    "r8a7795-h3ulcb-u-boot") &&
-				    dram_tiny.bank_num == 4 &&
-				    dram_tiny.bank_size == 0x40000000) {
+			    bank_num == 4 && bank_size == 0x40000000) {
 				return 0;
 			} else if (!strcmp(dt_fit.target_name,
-				   "r8a7795-h3ulcb-4x2g-u-boot") &&
-				   dram_tiny.bank_num == 4 &&
-				   dram_tiny.bank_size == 0x80000000) {
+					   "r8a7795-h3ulcb-4x2g-u-boot") &&
+				   bank_num == 4 && bank_size == 0x80000000) {
 				return 0;
 			}
 #endif
